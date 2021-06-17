2f4c580ae0691547e032b45a5724539dfdfd4d62
Ayah Ahmad <ayah.ash62@gmail.com>
2021-06-17 13:00:06 +0300

    try on mac

--- src/App.js
+++ src/App.js
@@ -1,73 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
 
-const cardArray=[
-  
-  {
-    name: 'buttercup',
-    img: 'images/buttercup.png'
-    
-  },
-  {
-    name: 'courage',
-    img: 'images/Courage3.png'
-    
-  },
-  {
-    name: 'dexter',
-    img: 'images/dexter.png'
-    
-  },
-  {
-    name: 'edd',
-    img: 'images/Edd1.png'
-    
-  },
-  {
-    name: 'foster',
-    img: 'images/coco.png'
-    
-  },
-  {
-    name: 'rugG',
-    img: 'images/lil.png'
-    
-  },
-  {
-    name: 'rugB',
-    img: 'images/phil.png'
-    
-  },
-  {
-    name: 'recess',
-    img: 'images/recess.png'
-    
-  },
+import React from "react";
+import Main from "./Components/Main";
+import "./App.css";
 
-]
-
-cost grid = document.querySelector.('.grid')
-
-function createBoard(){
-  for(let i=0; i< cardArray; i++)
-};
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Main />
     </div>
   );
 }
