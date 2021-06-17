68b88c434f0280c48e79a7cfe1d4eaaa8914ddbe
AyahAshkanani <ayah.ash62@gmail.com>
2021-06-17 17:15:31 +0300

    Memory game

---
+++ src/components/images/index.js
@@ -0,0 +1,94 @@
+import buttercup from "./buttercup.png";
+import coco from "./coco.png";
+import Courage3 from "./Courage3.png";
+import dexter from "./dexter.png";
+import Edd from "./Edd.png";
+import Lil from "./Lil.png";
+import Phil from "./Phil.png";
+import recess from "./recess.png";
+import flip from "./flip.png";
+
+export let images = [
+  {
+    name: "buttercup",
+    pic: buttercup,
+    flipped: false,
+  },
+  {
+    name: "coco",
+    pic: coco,
+    flipped: false,
+  },
+  {
+    name: "Courage3",
+    pic: Courage3,
+    flipped: false,
+  },
+  {
+    name: "dexter",
+    pic: dexter,
+    flipped: false,
+  },
+  {
+    name: "Edd",
+    pic: Edd,
+    flipped: false,
+  },
+  {
+    name: "Lil",
+    pic: Lil,
+    flipped: false,
+  },
+  {
+    name: "Phil",
+    pic: Phil,
+    flipped: false,
+  },
+  {
+    name: "recess",
+    pic: recess,
+    flipped: false,
+  },
+  {
+    name: "buttercup",
+    pic: buttercup,
+    flipped: false,
+  },
+  {
+    name: "coco",
+    pic: coco,
+    flipped: false,
+  },
+  {
+    name: "Courage3",
+    pic: Courage3,
+    flipped: false,
+  },
+  {
+    name: "dexter",
+    pic: dexter,
+    flipped: false,
+  },
+  {
+    name: "Edd",
+    pic: Edd,
+    flipped: false,
+  },
+  {
+    name: "Lil",
+    pic: Lil,
+    flipped: false,
+  },
+  {
+    name: "Phil",
+    pic: Phil,
+    flipped: false,
+  },
+  {
+    name: "recess",
+    pic: recess,
+    flipped: false,
+  },
+];
+
+export const blankCard = flip;
